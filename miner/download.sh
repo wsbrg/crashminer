01c81b3250bb6f8be6316f5e887b0f088906a7c6
Felix Weissberg <weissberg@tu-berlin.de>
2025-10-15 11:02:34 +0200

    Use function database that includes function names

--- miner/download.sh
+++ miner/download.sh
@@ -5,7 +5,7 @@ set -e
 if [ ! -d data/functions ]; then
   echo "[*] Download functions dataset"
   pushd data
-    wget https://tubcloud.tu-berlin.de/s/iJZnd7TKz3Br4gD/download/functions.tar.gz
+    wget 'https://tubcloud.tu-berlin.de/s/LfDST3tpsPMXZ37/download/functions.tar.gz'
     tar xzf functions.tar.gz
   popd
 fi
